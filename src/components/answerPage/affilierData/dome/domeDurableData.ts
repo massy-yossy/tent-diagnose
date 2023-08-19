dbfeeebd26103fadee6d5b4e5704a970792b37cd
massymassy <139942115+massy-yossy@users.noreply.github.com>
2023-08-19 22:27:26 +0900

    OnepollShelter complete

--- src/components/answerPage/affilierData/dome/domeDurableData.ts
+++ src/components/answerPage/affilierData/dome/domeDurableData.ts
@@ -2,31 +2,39 @@ import { affilierDataType } from "../../../../types/affilierDataType";
 
 export const domeDurableData: affilierDataType[] = [
   {
-    id:1,
+    id: 1,
     blandName: "Coleman",
-    tentName:"ツーリングドーム/ST ポールポケット式",
-    image:"https://thumbnail.image.rakuten.co.jp/@0_mall/sportsauthority/cabinet/img/1436/66244203.jpg?_ex=300x300",
-    amazon:"https://amzn.to/3E6auHH",
-    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fsportsauthority%2F66244203%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fsportsauthority%2Fi%2F10407127%2F",
-    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Falpen-group%2F7005103511.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_title"
+    tentName: "ツーリングドーム/ST ポールポケット式",
+    image:
+      "https://thumbnail.image.rakuten.co.jp/@0_mall/sportsauthority/cabinet/img/1436/66244203.jpg?_ex=300x300",
+    amazon: "https://amzn.to/3E6auHH",
+    rakuten:
+      "https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fsportsauthority%2F66244203%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fsportsauthority%2Fi%2F10407127%2F",
+    yahoo:
+      "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Falpen-group%2F7005103511.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_title",
   },
   {
-    id:2,
+    id: 2,
     blandName: "BUNDOK",
-    tentName:"ソロ ドーム 1 BDK-08",
-    image:"https://thumbnail.image.rakuten.co.jp/@0_mall/livinglinks/cabinet/bdk-08ob_1.jpg?_ex=300x300",
-    amazon:"https://amzn.to/3QKDpbC",
-    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Flivinglinks%2Fbdk-08%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Flivinglinks%2Fi%2F10011423%2F",
-    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fjoshin%2F4955985197268-36-36939.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+    tentName: "ソロ ドーム 1 BDK-08",
+    image:
+      "https://thumbnail.image.rakuten.co.jp/@0_mall/livinglinks/cabinet/bdk-08ob_1.jpg?_ex=300x300",
+    amazon: "https://amzn.to/3QKDpbC",
+    rakuten:
+      "https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Flivinglinks%2Fbdk-08%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Flivinglinks%2Fi%2F10011423%2F",
+    yahoo:
+      "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fjoshin%2F4955985197268-36-36939.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img",
   },
   {
-    id:3,
+    id: 3,
     blandName: "Thous Winds",
-    tentName:"テント ソロ ワンポールテント",
-    image:"https://thumbnail.image.rakuten.co.jp/@0_mall/kerosuke-rs/cabinet/item22/120230409080905.jpg?_ex=300x300",
-    amazon:"https://amzn.to/44hmyAg",
-    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fkerosuke-rs%2F20230409081152%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fkerosuke-rs%2Fi%2F10012995%2F",
-    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fhanohano7163%2F2022yr12mt18daywq069ic0btpfzhvp.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_title"
+    tentName: "テント ソロ ワンポールテント",
+    image:
+      "https://thumbnail.image.rakuten.co.jp/@0_mall/kerosuke-rs/cabinet/item22/120230409080905.jpg?_ex=300x300",
+    amazon: "https://amzn.to/44hmyAg",
+    rakuten:
+      "https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fkerosuke-rs%2F20230409081152%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fkerosuke-rs%2Fi%2F10012995%2F",
+    yahoo:
+      "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fhanohano7163%2F2022yr12mt18daywq069ic0btpfzhvp.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_title",
   },
-
-]
+];
--- src/components/answerPage/pages/onepoll/OnepollShelter.tsx
+++ src/components/answerPage/pages/onepoll/OnepollShelter.tsx
@@ -1,9 +1,116 @@
+import { FC, memo } from "react";
 
+import AnswerPageLayout from "../../layout/AnswerPageLayout";
+import { OnepollShelterData } from "../../affilierData/onepoll/OnepollShelterData";
+import { css } from "@emotion/react";
 
-const OnepollShelter = () => {
+const OnepollShelter: FC = memo(() => {
   return (
-    <div>OnepollShelter</div>
-  )
-}
+    <AnswerPageLayout
+      tentName="ワンポールテント"
+      image="./images/onepollTent.webp"
+      tentComment={
+        <>
+          夏のソロキャンプは、
+          <br />
+          ワンポールテントのシェルタータイプがおすすめです。
+        </>
+      }
+      features={
+        <>
+          <img src="./images/article/onepoll1.webp" alt="テント写真" />
+          ワンポールテントとは、真ん中の1本のポールで支える円錐型テントのことです。
+          <br />
+          とんがり屋根の可愛らしい見た目が特徴で、モノポールテントやティピーテントとも呼ばれています。
+          <br />
+          <br />
+          <h2 css={sectionTitle}>シェルターテントとは</h2>
+          <p css={sectionText}>
+            シェルターテントとは、フロアシート（床面）がないテントのことです。
+          </p>
+        </>
+      }
+      goodPoint={
+        <>
+          <img src="./images/article/onepoll2.webp" alt="テント写真" />
+          ワンポールテントのメリットは、以下の3つです。
+          <ul>
+            <li>設営が簡単</li>
+            <li>風に強い</li>
+            <li>幅広いアレンジが可能</li>
+          </ul>
+          ワンポールテントの設営は「
+          <span>テントを広げてメインポールを立てるだけ</span>
+          」なので、初心者の方でも簡単に設営できます。
+          <br />
+          キャンプ初心者にとって、テントの設営は難関といっても過言ではありません。
+          <br />
+          面倒な設営がラクになるのはワンポールテントの大きなメリットといえます。
+          <br />
+          <br />
+          また、ワンポールテントの形状にもよりますが、天敵と呼べる「風」に強いのも大きな特徴です。
+          <br />
+          より風に強いテントを選ぶ際は、面の数が多いワンポールテントを選びましょう。（四角形より六角形、さらに八角形）
+          <br />
+          <br />
+          ワンポールテントは、インナーテントや煙突、スカート付きなど、きせつに応じてアレンジできる点も大きなメリットのひとつです。
+        </>
+      }
+      importantPoint={
+        <>
+          <img src="./images/article/onepoll3.webp" alt="テント写真" />
+          ワンポールテントは比較的値段の安いタイプですが、あまりにも安すぎるテントは避けることをおすすめします。
+          <ul>
+            <li>耐久性がない</li>
+            <li>壊れやすい</li>
+            <li>破けやすい</li>
+          </ul>
+          など、安いテントには安いなりの理由があります。
+          <br />
+          とりあえず試しに1回行ってみたいという方にはおすすめですが、キャンプを趣味にしたいと考えている方にはおすすめできません。
+          <br />
+          <br />
+          本気で趣味にしたいと思っている方は、最低でも1万円以上のテントを購入するようにしましょう。
+          <br />
+          <br />
+          また、夏場はシェルターテントで通気性をアップさせることも重要ですが、雨による浸水や虫対策は万全にしておきましょう。
+          <br />
+          <br />
+          簡単な対策は、メッシュのインナーテントを使用する、コットなどを使って底上げをするなどが挙げられます。
+        </>
+      }
+      affilierData={OnepollShelterData}
+    />
+  );
+});
+const media600 = `
+@media screen and (max-width: 600px)
+`;
 
-export default OnepollShelter
+const sectionTitle = css`
+  font-size: 20px;
+  font-weight: bold;
+  background-color: #305a30;
+  width: fit-content;
+  color: #fff;
+  padding: 12px 18px;
+  text-align: center;
+  min-width: 237px;
+  box-sizing: border-box;
+  ${media600} {
+    font-size: 22px;
+    text-align: start;
+    width: 100%;
+    padding: 12px 14px;
+  }
+`;
+const sectionText = css`
+  padding: 34px 0 0 0;
+  font-size: 16px;
+  line-height: 180%;
+  ${media600} {
+    padding: 33px 0 0 0;
+    font-size: 15px;
+  }
+`;
+export default OnepollShelter;
