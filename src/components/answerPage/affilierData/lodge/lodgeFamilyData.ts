1987af44b2e19d40d6c26d8c9677768acacfb036
massymassy <139942115+massy-yossy@users.noreply.github.com>
2023-08-21 00:03:52 +0900

    lodgeFamily complete

---
+++ src/components/answerPage/affilierData/lodge/lodgeFamilyData.ts
@@ -0,0 +1,32 @@
+import { affilierDataType } from "../../../../types/affilierDataType";
+
+export const lodgeFamilyData: affilierDataType[] = [
+  {
+    id:1,
+    blandName: "ogawa",
+    tentName:"オーナーロッジ タイプ52R T/C [5人用]",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B08SJ25VP4&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/44fjSDy",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fesportveryfast%2Fcp-0114%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fesportveryfast%2Fi%2F10001586%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fmitsuyoshi%2Fcjp2-2253-0-0.html"
+  },
+  {
+    id:2,
+    blandName: "Naturehike",
+    tentName:"大型 2～6人用 ロッジ型",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0B51QJBP5&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/45hOQMq",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fnaturehike-direct%2Fcnh22zp004%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fnaturehike-direct%2Fi%2F10000108%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fgfcreek%2Fnh21zp0009.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+  {
+    id:3,
+    blandName: "S'more(スモア) ",
+    tentName:"Dolce Lodge ドルチェロッジ テント",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0BH4KTJ19&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/3E0GBZk",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fblack-beautiful-kuromi%2Ffmzygkicj43dcad2icyla7gk2a%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fblack-beautiful-kuromi%2Fi%2F10072063%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fmoco-moco%2Fwss-79shsyl1a0iq.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+
+]
---
+++ src/components/answerPage/pages/lodge/LodgeFamily.tsx
@@ -0,0 +1,55 @@
+import { FC, memo } from "react";
+
+import AnswerPageLayout from "../../layout/AnswerPageLayout";
+import { lodgeFamilyData } from "../../affilierData/lodge/lodgeFamilyData";
+
+const LodgeFamily: FC = memo(() => {
+  return (
+    <AnswerPageLayout
+      tentName="ロッジテント"
+      image="./images/tent-illustration/lodge.webp"
+      tentComment={
+        <>ファミリーキャンプでデザイン性を求めるなら、ロッジテントがおすすめ</>
+      }
+      features={
+        <>
+          <img src="./images/article/lodge1.webp" alt="ロッジテント" />
+          ロッジテントとは、絵本に出てくるような「家」のようなフォルムをしたテントです。
+          <br />
+          <br />
+          最近では見かける機会が少なくなりましたが、70年代まではロッジテントが主流だっため、少しレトロなヴィンテージ感があるのが特徴です。
+        </>
+      }
+      goodPoint={
+        <>
+          <img src="./images/article/lodge2.webp" alt="ロッジテント" />
+          ロッジテントの良いところは、以下の3つです。
+          <ul>
+            <li>設営が意外と簡単</li>
+            <li>天井が高く、生活しやすい</li>
+            <li>見た目がかわいい</li>
+          </ul>
+          ロッジテントは設営が困難に思えますが、構造自体はシンプルなのでコツを掴めばそこまで難しくありません。
+          <br />
+          <br />
+          頑丈な鉄骨で骨組みを造り、そこに幕をかぶせて立ち上げるだけなので、1人では難しいですが2～3人であれば簡単に問題なく設営できるでしょう。
+          <br />
+          <br />
+          また、ロッジテントの室内は、ワンポールテントの天井の高さとドームテントの広さを良いとこ取りしたようなイメージで、生活しやすい環境です。
+        </>
+      }
+      importantPoint={
+        <>
+          <img src="./images/article/lodge3.webp" alt="ロッジテント" />
+          ロッジテントのデメリットは、他のテントに比べて価格が高いことです。ファミリーキャンプ用の大型テントなので仕方ありませんが、安いテントが欲しいと考えている方にはネックになるでしょう。
+          <br />
+          <br />
+          また、大型テントなので、重量が重く嵩張（かさば）ることがあるので、注意が必要です。
+        </>
+      }
+      affilierData={lodgeFamilyData}
+    />
+  );
+});
+
+export default LodgeFamily;
