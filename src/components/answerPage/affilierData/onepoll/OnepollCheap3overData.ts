73bcb92da3c943b8b2425ab88684188cb34bde93
massymassy <139942115+massy-yossy@users.noreply.github.com>
2023-08-20 12:26:04 +0900

    onepollcheap3over complete & affilier imge adjustment

---
+++ src/components/answerPage/affilierData/onepoll/OnepollCheap3overData.ts
@@ -0,0 +1,31 @@
+import { affilierDataType } from "../../../../types/affilierDataType";
+
+export const OnepollCheap3overData: affilierDataType[] = [
+  {
+    id:1,
+    blandName: "DOD",
+    tentName:"ワンポールテントM 5人用",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B084QL541N&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/3P3iyPy",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fdailyuse0582%2F10014475%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fdailyuse0582%2Fi%2F10014475%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fveryfast%2Fbs-1144.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+  {
+    id:2,
+    blandName: "Coleman",
+    tentName:"4人用 ワイドティピー/3025グレージュ",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0BJPGRMTH&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/457ngBz",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fabarter%2Fzjde5iedvioesxvv4cg7awrbgm%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fabarter%2Fi%2F10025849%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fdowntown-store%2Fa-b0bjpgrmth-20230730.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+  {
+    id:3,
+    blandName: "LOGOS",
+    tentName:"ナバホ Tepee 300/400",
+    image:"https://thumbnail.image.rakuten.co.jp/@0_mall/esglobal/cabinet/mc/09870440/ounwyiub_1.jpg?_ex=300x300",
+    amazon:"https://amzn.to/45CiPym",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fesglobal%2Fvcfvxqhr9cwix60l6jqstnzhaq%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fesglobal%2Fi%2F11256074%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fmakana%2Fb09qpzc8ws.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_title"
+  },
+]
