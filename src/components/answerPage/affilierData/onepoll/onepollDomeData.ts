0e79e1c0f855ea307b1ab2a1f498ce38d4dc53b9
massymassy <139942115+massy-yossy@users.noreply.github.com>
2023-08-20 11:57:10 +0900

    onepollDome complete

---
+++ src/components/answerPage/affilierData/onepoll/onepollDomeData.ts
@@ -0,0 +1,31 @@
+import { affilierDataType } from "../../../../types/affilierDataType";
+
+export const onepollDomeData: affilierDataType[] = [
+  {
+    id:1,
+    blandName: "BUNDOK",
+    tentName:"ソロ ティピー BDK-75TC",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B086QTZVWS&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/3qE7mj3",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Flivinglinks%2Fbdk-75tc%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Flivinglinks%2Fi%2F10019191%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fshopping.yahoo.co.jp%2Fsearch%3Farea%3D13%26astk%3D%26aq%3D%26oq%3D%26first%3D1%26ss_first%3D1%26ts%3D1692455012%26mcr%3Dddd85ffe2b52457a14b89b18e5b2d4df%26tab_ex%3Dcommerce%26sretry%3D1%26p%3Dbundok%2B%25E3%2583%2586%25E3%2583%25B3%25E3%2583%2588%26sc_i%3Dshp_pc_search_searchBox_2"
+  },
+  {
+    id:2,
+    blandName: "ONETIGRIS",
+    tentName:"Northgaze ポリコットンTC ワンポールテント",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B09GLXLJPT&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/47HdwzQ",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fonetigris%2Fce-yzp09-tc%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fonetigris%2Fi%2F10000209%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fmoco-moco%2Fwss-316vebgsgeko.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+  {
+    id:3,
+    blandName: "Preself",
+    tentName:"2-4人用 ワンポールテント",
+    image:"https://ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B08PQFQGMC&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=ganso625701-22&language=ja_JP",
+    amazon:"https://amzn.to/47Bpr1Z",
+    rakuten:"https://af.moshimo.com/af/c/click?a_id=3719273&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fitem.rakuten.co.jp%2Fforest369%2Fs-b08pqfqgmc-20230115%2F&m=http%3A%2F%2Fm.rakuten.co.jp%2Fforest369%2Fi%2F10012768%2F",
+    yahoo: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3672470&pid=888362917&vc_url=https%3A%2F%2Fstore.shopping.yahoo.co.jp%2Fpurplestore%2F2022yr12mt18daywq069ic08pqfqgmc.html%3Fsc_i%3Dshp_pc_search_itemlist_shsrg_img"
+  },
+]
